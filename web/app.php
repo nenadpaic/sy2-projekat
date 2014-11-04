fbad413e951fb0adb81991d3b3fc75a3b01d5e55
Nenad <nenadpaic@gmail.com>
2014-11-04 11:50:24 +0100

    Resursi

--- web/app.php
+++ web/app.php
@@ -9,9 +9,9 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
 
-$apcLoader = new ApcClassLoader('sf2', $loader);
+/*$apcLoader = new ApcClassLoader('sf2', $loader);
 $loader->unregister();
-$apcLoader->register(true);
+$apcLoader->register(true);*/
 
 
 require_once __DIR__.'/../app/AppKernel.php';
